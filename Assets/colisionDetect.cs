16262f844ead8d10b88bd4c24c219275662f62f7
dzino87 <glock.jurga@gmail.com>
2020-06-05 19:42:40 +0200

    zadanie 1

---
+++ Assets/colisionDetect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class colisionDetect : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public Text gameScore;
+    public Text endScore;
+    // Start is called before the first frame update
+
+    private void Start()
+    
+    {
+        gameOverPanel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+
+    {
+        if (other.gameObject.tag == "box")
+        {
+            endScore.text = gameScore.text;
+            gameOverPanel.gameObject.SetActive(true);
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/pointCounter.cs
@@ -0,0 +1,31 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class pointCounter : MonoBehaviour
+{
+    public Text pointText;
+    private int points = 0;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        pointText.text = "score :" + points.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "box")
+        {
+            points += 1;
+            Destroy(other.gameObject);
+        }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+   
+}
